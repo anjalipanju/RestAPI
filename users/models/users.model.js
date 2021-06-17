3ac2906f75dacb9b757379078a0994540cc9e048
anjalipanju <45040570+anjalipanju@users.noreply.github.com>
2021-06-17 12:22:29 +0530

    Update users.model.js

--- users/models/users.model.js
+++ users/models/users.model.js
@@ -10,3 +10,11 @@ exports.createUser = (userData) => {
     const user = new User(userData);
     return user.save();
 };
+exports.findById = (id) => {
+    return User.findById(id).then((result) => {
+        result = result.toJSON();
+        delete result._id;
+        delete result.__v;
+        return result;
+    });
+};
