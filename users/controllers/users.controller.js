74091447411ba3097116c563d89196fd8a3e3174
anjalipanju <45040570+anjalipanju@users.noreply.github.com>
2021-06-17 12:24:35 +0530

    Update users.controller.js

--- users/controllers/users.controller.js
+++ users/controllers/users.controller.js
@@ -15,3 +15,16 @@ exports.getById = (req, res) => {
        res.status(200).send(result);
    });
 };
+exports.list = (req, res) => {
+   let limit = req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
+   let page = 0;
+   if (req.query) {
+       if (req.query.page) {
+           req.query.page = parseInt(req.query.page);
+           page = Number.isInteger(req.query.page) ? req.query.page : 0;
+       }
+   }
+   UserModel.list(limit, page).then((result) => {
+       res.status(200).send(result);
+   })
+};
