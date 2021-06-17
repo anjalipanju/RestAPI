e09f4680b853bf87e4b4f8ad228a7de869faf911
anjalipanju <45040570+anjalipanju@users.noreply.github.com>
2021-06-17 12:23:36 +0530

    Update users.model.js

--- users/models/users.model.js
+++ users/models/users.model.js
@@ -18,3 +18,8 @@ exports.findById = (id) => {
         return result;
     });
 };
+exports.patchUser = (id, userData) => {
+    return User.findOneAndUpdate({
+        _id: id
+    }, userData);
+};
